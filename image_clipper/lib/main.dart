54152613ca9be1fb8b4996b6bfbd5dee58af9d3a
Yamada Shunya <s-yamada@goldkey.co.jp>
2019-12-02 10:23:47 +0900

    Initial commit.

---
+++ image_clipper/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() => ImageClipperApp();
+
+class ImageClipperApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    MaterialApp(
+      title: 'Clipper',
+    );
+  }
+}
